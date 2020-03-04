f16732a27c6fa18459f8c683ad0ae9e7e08f96de
Charlie Birks <charlie@daftgames.net>
2020-03-04 10:33:16 +0000

    Pass a callback to JPEG decoding to make the outbuffer be allocated on the user heap

--- 32blit/engine/api_private.hpp
+++ 32blit/engine/api_private.hpp
@@ -12,6 +12,9 @@
 #include "../types/vec3.hpp"
 
 namespace blit {
+
+  using AllocateCallback = uint8_t *(*)(size_t);
+
   #pragma pack(push, 4)
   struct API {
     uint32_t buttons;
@@ -49,9 +52,9 @@ namespace blit {
     uint32_t (*get_max_us_timer)();
 
     // jepg
-    JPEGImage (*decode_jpeg_buffer)(uint8_t *ptr, uint32_t len);
-    JPEGImage (*decode_jpeg_file)(std::string filename);
-
+    JPEGImage (*decode_jpeg_buffer)(uint8_t *ptr, uint32_t len, AllocateCallback alloc);
+    JPEGImage (*decode_jpeg_file)(std::string filename, AllocateCallback alloc);
+  
   };
   #pragma pack(pop)
 
