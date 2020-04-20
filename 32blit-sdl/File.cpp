5dff0a69c1949a9ace95a7ef7bda846214f70c30
Charlie Birks <charlie@daftgames.net>
2020-04-20 12:12:20 +0100

    Attempt boost filesystem

--- 32blit-sdl/File.cpp
+++ 32blit-sdl/File.cpp
@@ -2,8 +2,8 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 #else
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
 #endif
 
 #include <string>
