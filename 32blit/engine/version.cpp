8ebcba935d24aa7d0939e737c7a4dbc99dcd8d76
Charlie Birks <charlie@daftgames.net>
2020-09-10 16:36:21 +0100

    Document version helpers

--- 32blit/engine/version.cpp
+++ 32blit/engine/version.cpp
@@ -3,9 +3,20 @@
 #include "api_private.hpp"
 
 namespace blit {
+  /**
+   * Get the version number of the library e.g. "v1.0.0"
+   * 
+   * \returns Version number string
+   */
   const char *get_version_string() {
     return BLIT_BUILD_VER;
   }
+
+  /**
+   * Get the build date of the library e.g. "2020-03-04"
+   * 
+   * \returns Build date string
+   */
   const char *get_build_date() {
     return BLIT_BUILD_DATE;
   }
