5223f14eda512638634a70f4e9168d754992c0ed
Charlie Birks <charlie@daftgames.net>
2020-11-12 18:44:26 +0000

    More install bits

---
+++ 32blit-sdl/32BlitConfig.cmake
@@ -0,0 +1,6 @@
+include(CMakeFindDependencyMacro)
+
+#find_dependency(SDL2 REQUIRED) something like this?
+
+include("${CMAKE_CURRENT_LIST_DIR}/32BlitTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/functions.cmake")
