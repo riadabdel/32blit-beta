c32d90a3f15fa6f4d1488062c0fd88de690a48ce
Charlie Birks <charlie@daft.games>
2023-03-07 23:19:17 +0000

    pico: move framebuffer allocation to the other side of teh API
    firmware now reserves 32k of RAM (may need less)

--- 32blit-pico/display.hpp
+++ 32blit-pico/display.hpp
@@ -9,9 +9,13 @@
 static const int lores_page_size = (DISPLAY_WIDTH / 2) * ((DISPLAY_HEIGHT + 1) / 2) * 2;
 
 extern blit::ScreenMode cur_screen_mode;
-#ifndef BLIT_BOARD_PIMORONI_PICOVISION
+
+#if defined(BUILD_LOADER) || defined(BLIT_BOARD_PIMORONI_PICOVISION)
+extern uint16_t *screen_fb;
+#else
 extern uint16_t screen_fb[];
 #endif
+
 extern uint16_t *screen_palette565;
 
 inline int get_display_page_size() {
@@ -37,3 +41,5 @@ blit::SurfaceInfo &set_screen_mode(blit::ScreenMode mode);
 bool set_screen_mode_format(blit::ScreenMode new_mode, blit::SurfaceTemplate &new_surf_template);
 
 void set_screen_palette(const blit::Pen *colours, int num_cols);
+
+void set_framebuffer(uint8_t *data, uint32_t max_size);
