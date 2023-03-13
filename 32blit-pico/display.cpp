a56c40a0633f2c1c531e039b81f0bae811d52915
Charlie Birks <charlie@daft.games>
2023-03-13 20:33:59 +0000

    pico: calculate page size

--- 32blit-pico/display.cpp
+++ 32blit-pico/display.cpp
@@ -41,10 +41,7 @@ static void init_palette() {
 }
 
 int get_display_page_size() {
-  if(cur_screen_mode == blit::ScreenMode::lores) // paletted is half the size
-    return blit::screen.format == blit::PixelFormat::P ? lores_page_size / 2 : lores_page_size;
-  else // paletted hires
-    return DISPLAY_WIDTH * DISPLAY_HEIGHT;
+  return cur_surf_info.bounds.area() * pixel_format_stride[int(cur_surf_info.format)];
 }
 
 // blit api
