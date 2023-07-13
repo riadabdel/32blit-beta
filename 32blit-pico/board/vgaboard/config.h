27025471ab74c8b43e41d5f10147244f0fba8e08
Charlie Birks <charlie@daft.games>
2023-07-13 21:00:02 +0100

    pico: move native SD pins to config.h

--- 32blit-pico/board/vgaboard/config.h
+++ 32blit-pico/board/vgaboard/config.h
@@ -4,6 +4,11 @@
 #define ALLOW_HIRES 0 // disable by default, mode switching isn't supported
 #endif
 
+// native
+#define SD_CLK   5
+#define SD_CMD  18
+#define SD_DAT0 19
+
 // spi
 #define SD_SCK   5
 #define SD_MOSI 18
