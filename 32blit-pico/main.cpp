1431d680977f3699b9f1e490cc85ac7f0349f684
Charlie Birks <charlie@daft.games>
2023-07-13 17:27:36 +0100

    HACK: pico: less overclock
    PicoVision doesn't like 250

--- 32blit-pico/main.cpp
+++ 32blit-pico/main.cpp
@@ -277,7 +277,7 @@ int main() {
   // this is required for a stable 250MHz on some RP2040s
   vreg_set_voltage(VREG_VOLTAGE_1_20);
   sleep_ms(10);
-  set_sys_clock_khz(250000, false);
+  set_sys_clock_khz(236000, true);
 #endif
 
   stdio_init_all();
