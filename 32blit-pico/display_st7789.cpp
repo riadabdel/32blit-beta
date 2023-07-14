cdd4c85bf0ad01c4221a786242300580efce84d4
Charlie Birks <charlie@daft.games>
2023-07-14 16:58:41 +0100

    pico: restore pallete support
    Deliberately omitted in the previous commit

--- 32blit-pico/display_st7789.cpp
+++ 32blit-pico/display_st7789.cpp
@@ -69,7 +69,7 @@ bool display_render_needed() {
 }
 
 bool display_mode_supported(blit::ScreenMode new_mode, const blit::SurfaceTemplate &new_surf_template) {
-  return new_surf_template.format == blit::PixelFormat::RGB565;
+  return new_surf_template.format == blit::PixelFormat::RGB565 || new_surf_template.format == blit::PixelFormat::P;
 }
 
 void display_mode_changed(blit::ScreenMode new_mode, blit::PixelFormat new_format) {
