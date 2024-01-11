c320c2cfe9f174f646a08269378448ab4b6df1a9
Charlie Birks <charlie@daft.games>
2024-01-11 17:49:07 +0000

    pico: GBA input

--- 32blit-pico/board/gbacart/config.cmake
+++ 32blit-pico/board/gbacart/config.cmake
@@ -1,3 +1,4 @@
 set(BLIT_BOARD_NAME "GBA Cart")
 
 blit_driver(display gbacart)
+blit_driver(input gbacart)
---
+++ 32blit-pico/input_gbacart.cpp
@@ -0,0 +1,27 @@
+#include "input.hpp"
+
+#include "engine/api_private.hpp"
+#include "engine/input.hpp"
+
+#include "gbacart.h"
+
+void init_input() {
+}
+
+void update_input() {
+  using namespace blit;
+
+  auto cart_api = gbacart_get_api();
+  auto button_state = ~cart_api->buttons;
+
+  api.buttons = ((button_state & (1 << 5)) ? uint32_t(Button::DPAD_LEFT) : 0)
+              | ((button_state & (1 << 4)) ? uint32_t(Button::DPAD_RIGHT) : 0)
+              | ((button_state & (1 << 6)) ? uint32_t(Button::DPAD_UP) : 0)
+              | ((button_state & (1 << 7)) ? uint32_t(Button::DPAD_DOWN) : 0)
+              | ((button_state & (1 << 0)) ? uint32_t(Button::A) : 0)
+              | ((button_state & (1 << 1)) ? uint32_t(Button::B) : 0)
+              | ((button_state & (1 << 9)) ? uint32_t(Button::X) : 0)  // L
+              | ((button_state & (1 << 8)) ? uint32_t(Button::Y) : 0)  // R 
+              | ((button_state & (1 << 2)) ? uint32_t(Button::MENU) : 0)  // Select
+              | ((button_state & (1 << 3)) ? uint32_t(Button::HOME) : 0); // Start
+}
