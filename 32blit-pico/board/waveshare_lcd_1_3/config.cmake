24fa96a4005030d7991ec0d2adf9bec3c4c71325
Charlie Birks <charlie@daft.games>
2024-02-02 20:50:06 +0000

    pico: add Waveshare Pico LCD 1.3 config
    The display is sideways though.

---
+++ 32blit-pico/board/waveshare_lcd_1_3/config.cmake
@@ -0,0 +1,4 @@
+set(BLIT_BOARD_NAME "Pico LCD 1.3")
+
+blit_driver(display st7789)
+blit_driver(input gpio)
---
+++ 32blit-pico/board/waveshare_lcd_1_3/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define BUTTON_UP_PIN 2
+#define BUTTON_DOWN_PIN 18
+#define BUTTON_LEFT_PIN 16
+#define BUTTON_RIGHT_PIN 20
+
+#define BUTTON_A_PIN 15
+#define BUTTON_B_PIN 17
+#define BUTTON_X_PIN 19
+#define BUTTON_Y_PIN 21
+
+// joystick press = 3
+
+#define LCD_CS_PIN 9
+#define LCD_DC_PIN 8
+#define LCD_SCK_PIN 10
+#define LCD_MOSI_PIN 11
+#define LCD_RESET_PIN 12
+#define LCD_BACKLIGHT_PIN 13
